008a144d6d77c44a4e84ee97f60d3fd5dfae6520
Isaac Holston <iholston@protonmail.com>
2022-05-19 03:12:34 +0000

    Create keepawake_time.ps1

---
+++ keepawake_time.ps1
@@ -0,0 +1,14 @@
+$TimeEnd = Get-Date -Hour 17 -Minute 30 -Second 00 # Enter the time you want the script to end here. Hours are from 0-24. 17:30 = 5:30pm.
+$WShell = New-Object -com "Wscript.Shell"
+while ($(Get-Date) -lt $TimeEnd)
+{
+  Clear-Host
+  Write-Host "keepalive.ps1 running until => $TimeEnd`n"
+  Write-Host “$($TimeEnd.Subtract($(Get-Date)).ToString(“hh' hours 'mm' minutes 'ss")) seconds remaining.”
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Seconds 60
+}
+Clear-Host
+Write-Host "$TimeEnd has passed. keepalive_time.ps1 exiting...`n"
