49f5dd8a41884c62fa558496f113f0ba9dd1d9dd
Isaac Holston <iholston@protonmail.com>
2022-05-19 03:13:48 +0000

    Updated comment and script name in write-host

--- keepawake_time.ps1
+++ keepawake_time.ps1
@@ -1,9 +1,9 @@
-$TimeEnd = Get-Date -Hour 17 -Minute 30 -Second 00 # Enter the time you want the script to end here. Hours are from 0-24. 17:30 = 5:30pm.
+$TimeEnd = Get-Date -Hour 17 -Minute 30 -Second 00 # Enter the time you want the script to end here. Hours are from 0-23. 17:30 = 5:30pm.
 $WShell = New-Object -com "Wscript.Shell"
 while ($(Get-Date) -lt $TimeEnd)
 {
   Clear-Host
-  Write-Host "keepalive.ps1 running until => $TimeEnd`n"
+  Write-Host "keepalive_time.ps1 running until => $TimeEnd`n"
   Write-Host “$($TimeEnd.Subtract($(Get-Date)).ToString(“hh' hours 'mm' minutes 'ss")) seconds remaining.”
   $WShell.sendkeys("{SCROLLLOCK}")
   Start-Sleep -Milliseconds 100
